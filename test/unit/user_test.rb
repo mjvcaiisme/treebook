fe390b87771acbe283896828db4a5acfb3571241
lucas <lucas@thinkpanda.com>
2013-01-30 15:01:35 -0500

    Added user friendships and associated tests.

--- test/unit/user_test.rb
+++ test/unit/user_test.rb
@@ -1,6 +1,9 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
+  should have_many(:user_friendships)
+  should have_many(:friends)
+
   test "a user should enter a first name" do
   	user = User.new
   	assert !user.save
@@ -48,4 +51,17 @@ class UserTest < ActiveSupport::TestCase
     user.profile_name = 'mjvcaiisme_2'
     assert user.valid?
   end  
+
+  test "that no error is raised when trying to access a friend list" do
+    assert_nothing_raised do
+      users(:lucas).friends
+    end  
+  end  
+
+  test "that creating friendships on a user works" do
+    users(:lucas).friends << users(:mike)
+    users(:lucas).friends.reload
+    assert users(:lucas).friends.include?(users(:mike))
+  end  
+   
 end
